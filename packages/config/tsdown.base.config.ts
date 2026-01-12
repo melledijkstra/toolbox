f6a9d6a89a56f9c44013d1c7f1ce2afa3fb9f17a
Melle Dijkstra <melle.dijkstra@adidas.com>
2026-01-12 17:43:53 +0100

    Add rollup bundle analyzer to tsdown process

--- packages/config/tsdown.base.config.ts
+++ packages/config/tsdown.base.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'tsdown'
+import { visualizer } from 'rollup-plugin-visualizer'
+
+const plugins = []
+
+if (process.env.ANALYZE === 'true') {
+  plugins.push(visualizer({
+    open: true,
+    filename: 'stats.html',
+    template: 'treemap',
+  }))
+}
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
+  plugins,
 })
